fa3836feb3270ff013cf6ad14f47ad4a94dd0b6d
NiceM126 <3048193080@qq.com>
2025-03-02 21:45:50 +0800

    初始化邮件服务

--- app/extensions.py
+++ app/extensions.py
@@ -1,13 +1,14 @@
+"""Flask扩展初始化"""
 from flask_cors import CORS
-from flask_assets import Environment
+from flask_mail import Mail
 
 # 初始化CORS
 cors = CORS()
 
-# 初始化Assets
-assets = Environment()
+# 初始化邮件服务
+mail = Mail()
 
 def init_extensions(app):
     """初始化所有Flask扩展"""
     cors.init_app(app)
-    assets.init_app(app)
+    mail.init_app(app)
