9c91ccf9cedca81d5a5cd65376f3e9dc709886e7
NiceM126 <3048193080@qq.com>
2025-03-23 04:23:18 +0800

    更新config

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import os
 from datetime import timedelta
 
+
 class Config:
     # 基本配置
+
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
     
     # Redis配置
@@ -21,3 +23,12 @@ class Config:
     # CSRF配置
     WTF_CSRF_ENABLED = True
     WTF_CSRF_CHECK_DEFAULT = False  # 只在表单提交时检查CSRF 
+
+    # 邮件服务器配置
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.gmail.com'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'your-email@gmail.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'your-app-password'
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER') or 'your-email@gmail.com'
+    CONTACT_EMAIL = os.environ.get('CONTACT_EMAIL') or 'your-email@gmail.com' 
